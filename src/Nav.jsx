cfc7a551cd5ff68b860be3ae7023992abda12227
gabr7887 <luizgabriel7887@gmail.com>
2024-09-29 12:32:32 -0300

    adicionado hover no menu desktop

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -13,10 +13,16 @@ const Nav = ({ active }) => {
       </div>
       <nav className={style.ulBg}>
         <ul className={style.navItems}>
-          <li className={active === 0 ? style.active : ''}>00 - HOME</li>
+          <li className={active === 0 ? style.active : ''}>
+            {active === 0 &&
+            window.innerWidth >= 530 &&
+            window.innerWidth <= 768
+              ? 'HOME'
+              : '00 - HOME'}
+          </li>
           <li className={active === 1 ? style.active : ''}>01 - DESTINATION</li>
-          <li className={active === 3 ? style.active : ''}>02 - CREW</li>
-          <li className={active === 4 ? style.active : ''}>03 - TECHNOLOGY</li>
+          <li className={active === 2 ? style.active : ''}> 02 - CREW</li>
+          <li className={active === 3 ? style.active : ''}>03 - TECHNOLOGY</li>
         </ul>
       </nav>
     </div>
