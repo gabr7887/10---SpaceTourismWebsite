4d7971fc1c7fa6a14f4af98879eeb0935fa554bb
gabr7887 <luizgabriel7887@gmail.com>
2024-09-30 18:58:56 -0300

    menu falta terminar responsividade

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -11,7 +11,16 @@ const Nav = ({ active }) => {
       <div className={style.detailDiv}>
         <p className={style.detail}> </p>
       </div>
+      <div className={style.hamburgerMenuDiv}>
+        <p className={style.menuBar}> </p>
+        <p className={style.menuBar}> </p>
+        <p className={style.menuBar}> </p>
+      </div>
       <nav className={style.ulBg}>
+        <div className={style.closeIcon}>
+          <p className={style.closeBar1}> </p>
+          <p className={style.closeBar2}> </p>
+        </div>
         <ul className={style.navItems}>
           <li className={active === 0 ? style.active : ''}>
             {active === 0 &&
@@ -20,9 +29,15 @@ const Nav = ({ active }) => {
               ? 'HOME'
               : '00 - HOME'}
           </li>
-          <li className={active === 1 ? style.active : ''}>01 - DESTINATION</li>
-          <li className={active === 2 ? style.active : ''}> 02 - CREW</li>
-          <li className={active === 3 ? style.active : ''}>03 - TECHNOLOGY</li>
+          <li className={active === 1 ? style.active : ''}>
+            <p>01 - DESTINATION</p>
+          </li>
+          <li className={active === 2 ? style.active : ''}>
+            <p>02 - CREW</p>
+          </li>
+          <li className={active === 3 ? style.active : ''}>
+            <p>03 - TECHNOLOGY</p>
+          </li>
         </ul>
       </nav>
     </div>
