c70d6490bb00df231f8e3b1af40a5a67cf0ca4bf
gabr7887 <luizgabriel7887@gmail.com>
2024-10-16 16:28:40 -0300

    finalizado estrutura inicial da pagina, falta responsividade

--- src/components/destination/Destination.jsx
+++ src/components/destination/Destination.jsx
@@ -14,24 +14,32 @@ const Destination = () => {
       title: 'Moon',
       text: "See our planet as you've never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
       index: 0,
+      distancia: '384,400 km',
+      tempo: '3 days',
     },
     {
       photo: Mars,
       title: 'Mars',
       text: "Don't forget to pack your hiking boots. You'll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It's two and a half times the size of Everest!",
       index: 1,
+      distancia: '225 MIL. km',
+      tempo: '9 months',
     },
     {
       photo: Europa,
       title: 'Europa',
       text: "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover's dream. With an icy surface, it's perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
       index: 2,
+      distancia: '628 MIL. km',
+      tempo: '3 years',
     },
     {
       photo: Titan,
       title: 'Titan',
       text: 'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
       index: 3,
+      distancia: '1.6 BIL. km',
+      tempo: '7 years',
     },
   ];
 
@@ -50,20 +58,39 @@ const Destination = () => {
           <div className={style.photoDiv}>
             <img src={itens[activeItem].photo} alt="foto do astro celeste" />
           </div>
-          <ul className={style.nav}>
-            {itens.map((item) => (
-              <li
-                key={item.index}
-                onClick={() => handleItem(item.index)}
-                className={`${style.navItem} ${
-                  activeItem === item.index ? style.active : ''
-                }`}
-              >
-                {item.title}
-              </li>
-            ))}
-          </ul>
-          <div className={style.content}></div>
+          <div className={style.right}>
+            <ul className={style.nav}>
+              {itens.map((item) => (
+                <li
+                  key={item.index}
+                  onClick={() => handleItem(item.index)}
+                  className={`${style.navItem} ${
+                    activeItem === item.index ? style.active : ''
+                  }`}
+                >
+                  {item.title}
+                </li>
+              ))}
+            </ul>
+            <div className={style.content}>
+              <h1 className={style.itemTitle}>{itens[activeItem].title}</h1>
+              <p className={style.itemText}>{itens[activeItem].text}</p>
+              <div className={style.infoDiv}>
+                <div className={style.infoSubDiv}>
+                  <h3 className={style.infoTitle}>AVG. DISTANCE</h3>
+                  <span className={style.infoValue}>
+                    {itens[activeItem].distancia}
+                  </span>
+                </div>
+                <div className={style.infoSubDiv}>
+                  <h3 className={style.infoTitle}>EST. TRAVEL TIME</h3>
+                  <span className={style.infoValue}>
+                    {itens[activeItem].tempo}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
