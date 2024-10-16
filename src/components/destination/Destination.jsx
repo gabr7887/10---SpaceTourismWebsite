0bc076c973a7c6a3620ae71f9a7b101c013d0114
gabr7887 <luizgabriel7887@gmail.com>
2024-10-16 15:35:52 -0300

    finalizada nav do destination

--- src/components/destination/Destination.jsx
+++ src/components/destination/Destination.jsx
@@ -12,24 +12,32 @@ const Destination = () => {
     {
       photo: Moon,
       title: 'Moon',
+      text: "See our planet as you've never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
       index: 0,
     },
     {
       photo: Mars,
       title: 'Mars',
+      text: "Don't forget to pack your hiking boots. You'll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It's two and a half times the size of Everest!",
       index: 1,
     },
     {
       photo: Europa,
       title: 'Europa',
+      text: "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover's dream. With an icy surface, it's perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
       index: 2,
     },
     {
       photo: Titan,
       title: 'Titan',
+      text: 'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
       index: 3,
     },
   ];
+
+  function handleItem(index) {
+    setActiveItem(index);
+  }
 
   return (
     <div className={style.container}>
@@ -39,10 +47,18 @@ const Destination = () => {
           <span>01</span> PICK YOUR DESTINATION
         </h1>
         <div className={style.mainContainer}>
-          <div className={style.photoDiv}></div>
+          <div className={style.photoDiv}>
+            <img src={itens[activeItem].photo} alt="foto do astro celeste" />
+          </div>
           <ul className={style.nav}>
             {itens.map((item) => (
-              <li key={item.index} className={style.navItem}>
+              <li
+                key={item.index}
+                onClick={() => handleItem(item.index)}
+                className={`${style.navItem} ${
+                  activeItem === item.index ? style.active : ''
+                }`}
+              >
                 {item.title}
               </li>
             ))}
